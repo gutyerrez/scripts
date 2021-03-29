d6fccd595e92ef5ed446fe27c1f45c47548aded8
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-03-28 21:46:24 -0300

    dev

--- main.sh
+++ main.sh
@@ -14,9 +14,13 @@ MINECRAFT_DIRECTORY="$MAIN_DIRECTORY/minecraft"
 
 PROXIES_DIRECTORY="$MINECRAFT_DIRECTORY/proxies"
 LOBBIES_DIRECTORY="$MINECRAFT_DIRECTORY/lobbies"
+
 MAIN_LOBBIES_DIRECTORY="$LOBBIES_DIRECTORY/main"
 LOGIN_LOBBIES_DIRECTORY="$LOBBIES_DIRECTORY/login"
 
 SERVERS_DIRECTORY="$MINECRAFT_DIRECTORY/servers"
+
+FACTIONS_DIRECTORY="$SERVERS_DIRECTORY/factions"
+RANKUP_DIRECTORY="$SERVERS_DIRECTORY/rankup"
 
 MINECRAFT_JAVA_FLAGS="-Dcom.mojang.eula.agree=true -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
---
+++ server/factions/factions.sh
@@ -0,0 +1,44 @@
+DIRECTORY_PATH="$(dirname ${0})"
+
+cd "$(dirname $(readlink -f ${0}))"
+
+source /home/cloud/scripts/main.sh
+
+if [ "$#" -eq 1 ]; then
+  SERVER_NAME=$1
+
+  j=$(screen -list | grep -P "[0-9]+\.$SERVER_NAME[ \t]+" | wc -l)
+
+  if [[ $j < 1 ]]; then
+    cd ${FACTIONS_DIRECTORY}/${SERVER_NAME}
+
+    yes | cp ${OUTPUT_DIRECTORY}/PaperSpigot.jar ${FACTIONS_DIRECTORY}/${SERVER_NAME}
+
+    if ! [[ -e "settings.json" ]]; then
+      cp ${CLOUD_DIRECTORY}/scripts/server/factions/settings.json ${FACTIONS_DIRECTORY}/${SERVER_NAME}
+    else
+      plugins=$(jq .plugins ${CLOUD_DIRECTORY}/scripts/server/factions/settings.json)
+      
+      jq ".plugins |= $plugins" settings.json > settings.tmp && mv settings.tmp settings.json
+    fi
+
+    plugins=$(jq .plugins[] settings.json)
+
+    # find ${FACTIONS_DIRECTORY}/${SERVER_NAME}/plugins/ -maxdepth 1 -type f -name "*.jar" -delete
+
+    for plugin in $plugins; do
+      plugin=${plugin//\"}
+
+      if [[ -e "${OUTPUT_DIRECTORY}/$plugin" ]]; then
+        yes | cp ${OUTPUT_DIRECTORY}/$plugin ${FACTIONS_DIRECTORY}/${SERVER_NAME}/plugins
+      else
+        echo -e "${COLOR_RED}Não foi possível localizar o plugin $plugin.${COLOR_RESET}"
+      fi
+    done
+
+    echo -e "${COLOR_GREEN}Ligando o factions em $(pwd)...${COLOR_RESET}"
+    screen -dmS ${SERVER_NAME} java ${MINECRAFT_JAVA_FLAGS} -Xms128M -Xmx8G -jar PaperSpigot.jar
+  else
+    echo -e "${COLOR_YELLOW}Servidor já está ligado.${COLOR_RESET}"
+  fi
+fi
