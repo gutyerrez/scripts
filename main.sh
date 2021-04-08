1fad1c63aca15f3b850dbfcf2c2061903d46c239
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-08 00:06:22 -0300

    dev

--- main.sh
+++ main.sh
@@ -27,6 +27,6 @@ BUILD_DIRECTORY="$SERVERS_DIRECTORY/build"
 
 APPLICATIONS_DIRECTORY="$MAIN_DIRECTORY/applications"
 
-NYRAH_DIRECTORY="$MINECRAFT/nyrah"
+NYRAH_DIRECTORY="$MINECRAFT_DIRECTORY/nyrah"
 
 MINECRAFT_JAVA_FLAGS="-Dcom.mojang.eula.agree=true -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
