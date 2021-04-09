68e7d6540cb5fc05e3f3437360e69dba3acc3885
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-09 10:27:17 -0300

    dev

--- server/nyrah/nyrah.sh
+++ server/nyrah/nyrah.sh
@@ -16,7 +16,7 @@ if [ "$#" -eq 1 ]; then
     yes | cp ${CLOUD_DIRECTORY}/scripts/server/nyrah/settings.json ${NYRAH_DIRECTORY}
     
     echo -e "${COLOR_GREEN}Ligando o ${SERVER_NAME} em $(pwd)...${COLOR_RESET}"
-    screen -dmSL ${SERVER_NAME} ./Nyrah
+    screen -dmS ${SERVER_NAME} ./Nyrah
   else
     echo -e "${COLOR_YELLOW}O ${SERVER_NAME} já está ligado.${COLOR_RESET}"
   fi
