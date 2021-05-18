36335c9296b6a313c361a741036d8df413695293
Gutyerrez <36475012+Gutyerrez@users.noreply.github.com>
2021-05-18 12:57:27 -0300

    Update login.sh

--- server/login/login.sh
+++ server/login/login.sh
@@ -37,7 +37,7 @@ if [ "$#" -eq 1 ]; then
     done
 
     echo -e "${COLOR_GREEN}Ligando o ${SERVER_NAME} em $(pwd)...${COLOR_RESET}"
-    screen -dmS ${SERVER_NAME} java ${MINECRAFT_JAVA_FLAGS} -Xms128M -Xmx2G -jar PaperSpigot.jar
+    screen -dmS ${SERVER_NAME} java ${MINECRAFT_JAVA_FLAGS} -Xms128M -Xmx4G -jar PaperSpigot.jar
   else
     echo -e "${COLOR_YELLOW}O ${SERVER_NAME} já está ligado.${COLOR_RESET}"
   fi
