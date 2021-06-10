34db8e5713b0a8426024780e707ab7530bc0ae11
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-06-10 16:47:21 -0300

    debug

--- server/factions/factions.sh
+++ server/factions/factions.sh
@@ -1,24 +1,18 @@
-# shellcheck disable=SC2034
-DIRECTORY_PATH="$(dirname "${0}")"
+DIRECTORY_PATH=$(dirname $0)
 
-# shellcheck disable=SC2046
-# shellcheck disable=SC2164
-cd "$(dirname $(readlink -f "${0}"))"
+cd $(dirname $(readlink -f $0))
 
 source /home/cloud/scripts/main.sh
 
 if [ "$#" -eq 1 ]; then
   SERVER_NAME=$1
 
-  # shellcheck disable=SC2126
   j=$(screen -list | grep -P "[0-9]+\.${SERVER_NAME}[ \t]+" | wc -l)
 
   if [[ $j -lt 1 ]]; then
-    # shellcheck disable=SC2164
-    cd "${FACTIONS_DIRECTORY}/${SERVER_NAME}"
+    cd $FACTIONS_DIRECTORY/$SERVER_NAME
 
-    # shellcheck disable=SC2216
-    yes | cp "${OUTPUT_DIRECTORY}/${MINECRAFT_SERVER_JAR}" "${FACTIONS_DIRECTORY}/${SERVER_NAME}"
+    yes | cp $OUTPUT_DIRECTORY/$MINECRAFT_SERVER_JAR $FACTIONS_DIRECTORY/$SERVER_NAME
 
     if [[ $SERVER_NAME == "factions-test" ]]; then
       # Only in test servers
@@ -28,7 +22,6 @@ if [ "$#" -eq 1 ]; then
       else
         plugins=$(jq .\"plugins-test\" "${CLOUD_DIRECTORY}/scripts/server/factions/settings.json")
         
-        # shellcheck disable=SC2216
         jq ".\"plugins-test\" |= $plugins" settings.json > settings.tmp && yes | mv settings.tmp settings.json
       fi
 
@@ -54,11 +47,11 @@ if [ "$#" -eq 1 ]; then
       # Only in test servers
     else
       if ! [[ -e "settings.json" ]]; then
-        cp "${CLOUD_DIRECTORY}/scripts/server/factions/settings.json" "${FACTIONS_DIRECTORY}/${SERVER_NAME}"
+        cp $CLOUD_DIRECTORY/scripts/server/factions/settings.json $FACTIONS_DIRECTORY/$SERVER_NAME
       else
         plugins=$(jq .plugins "${CLOUD_DIRECTORY}/scripts/server/factions/settings.json")
         
-        jq ".plugins |= $plugins" settings.json > settings.tmp && mv settings.tmp settings.json
+        jq ".plugins |= $plugins" settings.json > settings.tmp && yes | mv settings.tmp settings.json
       fi
 
       plugins=$(jq .plugins[] settings.json)
@@ -66,9 +59,8 @@ if [ "$#" -eq 1 ]; then
       for plugin in $plugins; do
         plugin=${plugin//\"}
 
-        if [[ -e "${OUTPUT_DIRECTORY}/$plugin" ]]; then
-          # shellcheck disable=SC2216
-          yes | cp "${OUTPUT_DIRECTORY}/$plugin" "${FACTIONS_DIRECTORY}/${SERVER_NAME}/plugins"
+        if [[ -e $OUTPUT_DIRECTORY/$plugin ]]; then
+          yes | cp $OUTPUT_DIRECTORY/$plugin $FACTIONS_DIRECTORY/$SERVER_NAME/plugins
         else
           echo -e "${COLOR_RED}Não foi possível localizar o plugin $plugin.${COLOR_RESET}"
         fi
@@ -78,7 +70,7 @@ if [ "$#" -eq 1 ]; then
       
       # Depois trocar o "paper" >> "${MINECRAFT_SERVER_JAR}"
       
-      screen -dmS "${SERVER_NAME}" java "${MINECRAFT_JAVA_FLAGS}" -Xms128M -Xmx8G -jar paper.jar
+      screen -dmS $SERVER_NAME java $MINECRAFT_JAVA_FLAGS -Xms128M -Xmx8G -jar paper.jar
     fi
   else
     echo -e "${COLOR_YELLOW}O ${SERVER_NAME} já está ligado.${COLOR_RESET}"
--- server/lobbies/lobby.sh
+++ server/lobbies/lobby.sh
@@ -1,50 +1,43 @@
-# shellcheck disable=SC2034
-DIRECTORY_PATH="$(dirname "${0}")"
+DIRECTORY_PATH=$(dirname $0)
 
-# shellcheck disable=SC2046
-# shellcheck disable=SC2164
-cd "$(dirname $(readlink -f "${0}"))"
+cd $(dirname $(readlink -f $0))
 
 source /home/cloud/scripts/main.sh
 
 if [ "$#" -eq 1 ]; then
   SERVER_NAME=$1
 
-  # shellcheck disable=SC2126
   j=$(screen -list | grep -P "[0-9]+\.${SERVER_NAME}[ \t]+" | wc -l)
 
   if [[ $j -lt 1 ]]; then
-    # shellcheck disable=SC2164
-    cd "${MAIN_LOBBIES_DIRECTORY}/${SERVER_NAME}"
+    cd $MAIN_LOBBIES_DIRECTORY/$SERVER_NAME
 
-    # shellcheck disable=SC2216
-    yes | cp "${OUTPUT_DIRECTORY}/${MINECRAFT_SERVER_JAR}" "${MAIN_LOBBIES_DIRECTORY}/${SERVER_NAME}"
+    yes | cp $OUTPUT_DIRECTORY/$MINECRAFT_SERVER_JAR $MAIN_LOBBIES_DIRECTORY/$SERVER_NAME
 
     if ! [[ -e "settings.json" ]]; then
-      cp "${CLOUD_DIRECTORY}/scripts/server/lobbies/settings.json" "${MAIN_LOBBIES_DIRECTORY}/${SERVER_NAME}"
+      cp $CLOUD_DIRECTORY/scripts/server/lobbies/settings.json $MAIN_LOBBIES_DIRECTORY/$SERVER_NAME
     else
-      plugins=$(jq .plugins "${CLOUD_DIRECTORY}/scripts/server/lobbies/settings.json")
+      plugins=$(jq .plugins $CLOUD_DIRECTORY/scripts/server/lobbies/settings.json)
 
       jq ".plugins |= $plugins" settings.json >settings.tmp && mv settings.tmp settings.json
     fi
 
     plugins=$(jq .plugins[] settings.json)
 
-    find "${MAIN_LOBBIES_DIRECTORY}/${SERVER_NAME}/plugins/" -maxdepth 1 -type f -name "*.jar" -delete
+    find $MAIN_LOBBIES_DIRECTORY/$SERVER_NAME/plugins/ -maxdepth 1 -type f -name "*.jar" -delete
 
     for plugin in ${plugins}; do
       plugin=${plugin//\"/}
 
-      if [[ -e "${OUTPUT_DIRECTORY}/${plugin}" ]]; then
-        # shellcheck disable=SC2216
-        yes | cp "${OUTPUT_DIRECTORY}/${plugin}" "${MAIN_LOBBIES_DIRECTORY}/${SERVER_NAME}/plugins"
+      if [[ -e $OUTPUT_DIRECTORY/$plugin ]]; then
+        yes | cp $OUTPUT_DIRECTORY/$plugin $MAIN_LOBBIES_DIRECTORY/$SERVER_NAME/plugins
       else
         echo -e "${COLOR_RED}Não foi possível localizar o plugin ${plugin}.${COLOR_RESET}"
       fi
     done
 
     echo -e "${COLOR_GREEN}Ligando o ${SERVER_NAME} em $(pwd)...${COLOR_RESET}"
-    screen -dmS "${SERVER_NAME}" java "${MINECRAFT_JAVA_FLAGS}" -Xms128M -Xmx3G -jar "${MINECRAFT_SERVER_JAR}"
+    screen -dmS $SERVER_NAME java $MINECRAFT_JAVA_FLAGS -Xms128M -Xmx3G -jar $MINECRAFT_SERVER_JAR
   else
     echo -e "${COLOR_YELLOW}O ${SERVER_NAME} já está ligado.${COLOR_RESET}"
   fi
