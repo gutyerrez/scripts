dfe11dfa24b559b1edab341d12deb3aec438c327
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-06-10 18:06:50 -0300

    test

--- server/proxies/proxy.sh
+++ server/proxies/proxy.sh
@@ -1,36 +1,36 @@
-DIRECTORY_PATH=$(dirname $0)
+DIRECTORY_PATH="$(dirname ${0})"
 
-cd $(dirname $(readlink -f $0))
+cd "$(dirname $(readlink -f ${0}))"
 
 source /home/cloud/scripts/main.sh
 
 if [ "$#" -eq 1 ]; then
   SERVER_NAME=$1
 
-  j=$(screen -list | grep -P "[0-9]+\.${SERVER_NAME}[ \t]+" | wc -l)
+  j=$(screen -list | grep -P "[0-9]+\.$SERVER_NAME[ \t]+" | wc -l)
 
-  if [[ $j -lt 1 ]]; then
-    cd $PROXIES_DIRECTORY/$SERVER_NAME
+  if [[ $j < 1 ]]; then
+    cd ${PROXIES_DIRECTORY}/${SERVER_NAME}
 
-    yes | cp $OUTPUT_DIRECTORY/$PROXY_SERVER_JAR $PROXIES_DIRECTORY/$SERVER_NAME
+    yes | cp ${OUTPUT_DIRECTORY}/Waterfall.jar ${PROXIES_DIRECTORY}/${SERVER_NAME}
 
     if ! [[ -e "settings.json" ]]; then
-      cp $CLOUD_DIRECTORY/scripts/server/proxies/settings.json $PROXIES_DIRECTORY/$SERVER_NAME
+      cp ${CLOUD_DIRECTORY}/scripts/server/proxies/settings.json ${PROXIES_DIRECTORY}/${SERVER_NAME}
     else
-      plugins=$(jq -c .plugins $CLOUD_DIRECTORY/scripts/server/proxies/settings.json)
+      plugins=$(jq -c .plugins ${CLOUD_DIRECTORY}/scripts/server/proxies/settings.json)
 
-      jq -c ".plugins |= $plugins" settings.json >settings.tmp && mv settings.tmp settings.json
+      jq -c ".plugins |= $plugins" settings.json > settings.tmp && mv settings.tmp settings.json
     fi
 
     plugins=$(jq .plugins[] settings.json)
 
-    find $PROXIES_DIRECTORY/$SERVER_NAME/plugins/ -maxdepth 1 -type f -name "*.jar" -delete
+    find ${PROXIES_DIRECTORY}/${SERVER_NAME}/plugins/ -maxdepth 1 -type f -name "*.jar" -delete
 
     for plugin in $plugins; do
-      if [[ -e "$OUTPUT_DIRECTORY/$plugin" ]]; then
-        yes | cp $OUTPUT_DIRECTORY/$plugin $PROXIES_DIRECTORY/$SERVER_NAME/$plugins
+      if [[ -e "${OUTPUT_DIRECTORY}/$plugin" ]]; then
+        yes | cp ${OUTPUT_DIRECTORY}/$plugin ${PROXIES_DIRECTORY}/${SERVER_NAME}/plugins
       else
-        echo -e "${COLOR_RED}Não foi possível localizar o plugin ${plugin}.${COLOR_RESET}"
+        echo -e "${COLOR_RED}Não foi possível localizar o plugin $plugin.${COLOR_RESET}"
       fi
     done
 
