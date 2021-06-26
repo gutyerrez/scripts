f993967d22a308a290e4911d90f15d6a998acc0c
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-06-26 08:51:12 -0300

    dev

--- services/github/actions/deploy.sh
+++ services/github/actions/deploy.sh
@@ -7,7 +7,7 @@ source /home/cloud/scripts/main.sh
 while read -r HOST; do
   for FILE in $@; do
     if [[ $FILE -eq "scripts" ]]; then
-      scp -o StrictHostKeyChecking=no -r "$GITHUB_WORKSPACE" root@$HOST:/home/cloud/scripts
+      scp -o StrictHostKeyChecking=no -r "$GITHUB_WORKSPACE/" root@$HOST:/home/cloud/scripts
     else
       scp -o StrictHostKeyChecking=no -r "$GITHUB_WORKSPACE/$FILE" root@$HOST:/home/cloud/output
     fi
