8c4324d4783f792dadba28efc347bcbbce4d8232
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-07-17 15:37:51 -0300

    dev

--- main.sh
+++ main.sh
@@ -7,32 +7,40 @@ HOSTS=(
   "135.148.70.166"
 )
 
+# main folder
+
 MAIN_DIRECTORY="/home"
+
+# sub folders
 
 CLOUD_DIRECTORY="$MAIN_DIRECTORY/cloud"
 OUTPUT_DIRECTORY="$MAIN_DIRECTORY/cloud/output"
 
 MINECRAFT_DIRECTORY="$MAIN_DIRECTORY/minecraft"
 
+# servers folders
+
+NYRAH_DIRECTORY="$MINECRAFT_DIRECTORY/nyrah"
+
 PROXIES_DIRECTORY="$MINECRAFT_DIRECTORY/proxies"
 LOBBIES_DIRECTORY="$MINECRAFT_DIRECTORY/lobbies"
-
-MAIN_LOBBIES_DIRECTORY="$LOBBIES_DIRECTORY/main"
-LOGIN_LOBBIES_DIRECTORY="$LOBBIES_DIRECTORY/login"
 
 SERVERS_DIRECTORY="$MINECRAFT_DIRECTORY/servers"
 
 FACTIONS_DIRECTORY="$SERVERS_DIRECTORY/factions"
-RANKUP_DIRECTORY="$SERVERS_DIRECTORY/rankup"
 
 BUILD_DIRECTORY="$SERVERS_DIRECTORY/build"
 
+# applications folders
+
 APPLICATIONS_DIRECTORY="$MAIN_DIRECTORY/applications"
 
-NYRAH_DIRECTORY="$MINECRAFT_DIRECTORY/nyrah"
+# java flags
+
+MINECRAFT_JAVA_FLAGS="-Dcom.mojang.eula.agree=true -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
+
+# jars
 
 NYRAH_CONTROL_SERVER_JAR="nyrah-control.jar"
 PROXY_SERVER_JAR="waterfall.jar"
 MINECRAFT_SERVER_JAR="paper-spigot.jar"
-
-MINECRAFT_JAVA_FLAGS="-Dcom.mojang.eula.agree=true -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
--- server/lobbies/lobby.sh
+++ server/lobbies/lobby.sh
@@ -10,12 +10,12 @@ if [ "$#" -eq 1 ]; then
   j=$(screen -list | grep -P "[0-9]+\.${SERVER_NAME}[ \t]+" | wc -l)
 
   if [[ $j -lt 1 ]]; then
-    cd $MAIN_LOBBIES_DIRECTORY/$SERVER_NAME
+    cd $LOBBIES_DIRECTORY/$SERVER_NAME
 
-    yes | cp ${OUTPUT_DIRECTORY}/${MINECRAFT_SERVER_JAR} ${MAIN_LOBBIES_DIRECTORY}/${SERVER_NAME}
+    yes | cp $OUTPUT_DIRECTORY/$MINECRAFT_SERVER_JAR $LOBBIES_DIRECTORY/$SERVER_NAME
 
     if ! [[ -e "settings.json" ]]; then
-      cp $CLOUD_DIRECTORY/scripts/server/lobbies/settings.json $MAIN_LOBBIES_DIRECTORY/$SERVER_NAME
+      cp $CLOUD_DIRECTORY/scripts/server/lobbies/settings.json $LOBBIES_DIRECTORY/$SERVER_NAME
     else
       plugins=$(jq .plugins $CLOUD_DIRECTORY/scripts/server/lobbies/settings.json)
 
@@ -24,18 +24,18 @@ if [ "$#" -eq 1 ]; then
 
     plugins=$(jq -r .plugins[] settings.json)
 
-    find $MAIN_LOBBIES_DIRECTORY/$SERVER_NAME/plugins/ -maxdepth 1 -type f -name "*.jar" -delete
+    find $LOBBIES_DIRECTORY/$SERVER_NAME/plugins/ -maxdepth 1 -type f -name "*.jar" -delete
 
     for plugin in $plugins; do
       if [[ -e $OUTPUT_DIRECTORY/$plugin ]]; then
-        yes | cp $OUTPUT_DIRECTORY/$plugin $MAIN_LOBBIES_DIRECTORY/$SERVER_NAME/plugins
+        yes | cp $OUTPUT_DIRECTORY/$plugin $LOBBIES_DIRECTORY/$SERVER_NAME/plugins
       else
         echo -e "${COLOR_RED}Não foi possível localizar o plugin ${plugin}.${COLOR_RESET}"
       fi
     done
 
     echo -e "${COLOR_GREEN}Ligando o ${SERVER_NAME} em $(pwd)...${COLOR_RESET}"
-    screen -dmS ${SERVER_NAME} java ${MINECRAFT_JAVA_FLAGS} -Xms128M -Xmx3G -jar ${MINECRAFT_SERVER_JAR}
+    screen -dmS $SERVER_NAME java $MINECRAFT_JAVA_FLAGS -Xms128M -Xmx3G -jar $MINECRAFT_SERVER_JAR
   else
     echo -e "${COLOR_YELLOW}O ${SERVER_NAME} já está ligado.${COLOR_RESET}"
   fi
