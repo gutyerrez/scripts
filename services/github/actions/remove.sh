82d7f40202d8cd2373b5811119d845834b14b861
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-07-17 17:16:26 -0300

    dev

--- services/github/actions/remove.sh
+++ services/github/actions/remove.sh
@@ -8,8 +8,6 @@ FILES=("$@")
 
 while read -r HOST; do
   for FILE in "${FILES[@]}"; do
-    echo -e "Remove file $FILE"
-
     ssh -o StrictHostKeyChecking=no root@$HOST rm -rf $FILE
   done
 done <<< $HOSTS
