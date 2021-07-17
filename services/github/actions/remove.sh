b6095b42bf6a96209a5ea9ecd2575a34dc80c588
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-07-17 16:53:57 -0300

    debug

--- services/github/actions/remove.sh
+++ services/github/actions/remove.sh
@@ -6,6 +6,8 @@ source /home/cloud/scripts/main.sh
 
 while read -r HOST; do
   for FILE in $@; do
+    echo -e "Remove file $FILE"
+
     ssh -o StrictHostKeyChecking=no root@$HOST rm -rf $FILE
   done
 done <<< $HOSTS
